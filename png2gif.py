cf20802ad6083a2de9e3442bf44ba219d93315fc
bqz913 <bqz913@eis.hokudai.ac.jp>
2021-01-11 22:58:07 +0900

    add png2gif.py

---
+++ png2gif.py
@@ -0,0 +1,11 @@
+import glob
+from PIL import Image
+
+def mkgif(name):
+    files = glob.glob(f'{name}/*.png')
+    images = list(map(lambda file: Image.open(file), files))
+
+    images[0].save(f'{name}.gif', save_all=True, append_images=images[1:], duration=20, loop=0)
+
+if __name__ == "__main__":
+    mkgif("frames")
